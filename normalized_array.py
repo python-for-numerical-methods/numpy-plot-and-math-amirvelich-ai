4cdf795370b19f9766ab07c80f1d632fd9a79d63
amirvelich-ai <amirvelich@gmail.com>
2026-04-03 16:32:27 +0300

    Update normalized_array.py

--- normalized_array.py
+++ normalized_array.py
@@ -1,24 +1,18 @@
 import numpy as np
 
-def normalized_array(data):
-    """
-    מנרמלת מערך נתונים לטווח של [0, 1] לפי שיטת Min-Max Scaling.
+def normalize_array(input_array):
+    data = np.array(input_array)
     
-    הנוסחה לביצוע:
-    x_norm = (x - min) / (max - min)
+    if np.all(data == data[0]):
+        return np.zeros(data.shape)
+    else:
+        new_array = (data - np.min(data)) / (np.max(data) - np.min(data))
     
-    פרמטרים:
-    data (list or np.array): מערך של מספרים.
-    
-    מחזירה:
-    np.array: מערך מנורמל. אם כל הערכים במערך זהים, יש להחזיר מערך של אפסים.
-    """
-    # המרת הקלט ל-numpy array לצורך חישובים וקטוריים
-    data = np.array(data)
-    
-    # --- כיתבו את הקוד שלכם כאן ---
-    pass
-    # חשוב לזכור להחליף את pass ב- return
+    return new_array
+
+
+print(normalize_array([5, 5, 5]))
+print(normalize_array([-1, 0, 1, 2]))
 
 if __name__ == "__main__":
     # כאן הסטודנטים יכולים להריץ בדיקה עצמית מהירה
