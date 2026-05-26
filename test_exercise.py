9935a30af6cc81974271edf2fdedc6aa80fb939d
amirvelich-ai <amirvelich@gmail.com>
2026-05-26 15:55:28 +0300

    Update test_exercise.py

--- test_exercise.py
+++ test_exercise.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 from normalized_array import normalized_array  # מייבא את הפונקציה מהקובץ של הסטודנט
 
-def test_basic_normalization()
+def test_basic_normalization():
     data = [10, 20, 30]
     result = normalized_array(data)
     expected = np.array([0.0, 0.5, 1.0])
